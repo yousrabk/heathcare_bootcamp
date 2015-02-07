f9c24598fe18333053880dc722681c6ff0339aca
Alexandre Gramfort <alexandre.gramfort@m4x.org>
2015-02-07 20:36:46 +0100

    rf300most_frequent

--- model.py
+++ model.py
@@ -3,8 +3,8 @@ from sklearn.preprocessing import Imputer
 from sklearn.pipeline import Pipeline
 
 def model(X_train, y_train, X_test):
-    clf = Pipeline([('imputer', Imputer()),
-                    ('rf', RandomForestClassifier(n_estimators=300))])
+    clf = Pipeline([('imputer', Imputer(strategy='most_frequent')),
+        ('rf', RandomForestClassifier(n_estimators=300))])
     clf.fit(X_train, y_train)
     y_pred = clf.predict(X_test)
     y_score = clf.predict_proba(X_test)
