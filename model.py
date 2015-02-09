1db702d1a14da32dc7a30a4c2feb97f160a81042
Adrien PAIN <adrien.pain@gmail.com>
2015-02-09 15:39:55 +0100

    same commit

--- model.py
+++ model.py
@@ -5,7 +5,7 @@ from sklearn.decomposition import RandomizedPCA
 
 def model(X_train, y_train, X_test):
     clf = Pipeline([('imputer', Imputer(strategy='most_frequent')),
-                    ('gbc_RF', GradientBoostingClassifier(n_estimators=100))])
+                    ('gbc', GradientBoostingClassifier(n_estimators=100))])
     clf.fit(X_train, y_train)
     y_pred = clf.predict(X_test)
     y_score = clf.predict_proba(X_test)
