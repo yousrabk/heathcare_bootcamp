e50cac36fd05fa603c182a914d80639b465999b6
Yousra Bekhti <yousra.bekhti@gmail.com>
2015-02-09 14:08:04 +0100

    gradientBoost

--- model.py
+++ model.py
@@ -5,8 +5,7 @@ from sklearn.decomposition import RandomizedPCA
 
 def model(X_train, y_train, X_test):
     clf = Pipeline([('imputer', Imputer(strategy='most_frequent')),
-                    ('rf', RandomForestClassifier(n_estimators=100,
-                                                  n_jobs=-1))])
+                    ('gbc_RF', GradientBoostingClassifier(n_estimators=100))])
     clf.fit(X_train, y_train)
     y_pred = clf.predict(X_test)
     y_score = clf.predict_proba(X_test)
