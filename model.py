af611b4a30e96c40bedd16688dc1fbcc12bcd83e
Yousra Bekhti <yousra.bekhti@gmail.com>
2015-02-09 13:30:43 +0100

    RFn100_mf

--- model.py
+++ model.py
@@ -1,10 +1,12 @@
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import Imputer
 from sklearn.pipeline import Pipeline
+from sklearn.decomposition import RandomizedPCA
 
 def model(X_train, y_train, X_test):
     clf = Pipeline([('imputer', Imputer(strategy='most_frequent')),
-        ('rf', RandomForestClassifier(n_estimators=40))])
+                    ('rf', RandomForestClassifier(n_estimators=100,
+                                                  n_jobs=-1))])
     clf.fit(X_train, y_train)
     y_pred = clf.predict(X_test)
     y_score = clf.predict_proba(X_test)
